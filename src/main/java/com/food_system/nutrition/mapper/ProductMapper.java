cd627fcd2bfb2c0d598d24efa0994f66522c4bd3
Yorik-coder <yorik656u@gmail.com>
2026-03-04 14:37:04 +0300

    message

--- src/main/java/com/food_system/nutrition/mapper/ProductMapper.java
+++ src/main/java/com/food_system/nutrition/mapper/ProductMapper.java
@@ -1,7 +1,6 @@
 package com.food_system.nutrition.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import com.food_system.nutrition.dto.ProductDto;
 import com.food_system.nutrition.entity.Product;
 
